d2ff37e6e08930afd28fbcdd48731815dfe6df20
Raghu Ramireddy <Raghu Ramireddy>
2015-12-21 14:25:49 +0530

    this is firstcommit..............

---
+++ Product.java
@@ -0,0 +1,30 @@
+public class Product
+{
+	private int productId;
+	private String productName;
+	private double productPrice;
+	public void setProductId(int productId)
+	{
+		this.productId=productId;
+	}
+	public int getProductId()
+	{
+		return productId;
+	}
+	public void setProductName(String productName)
+	{
+		this.productName=productName;
+	}
+	public String getProductName()
+	{
+		return productName;
+	}
+	public void setProductPrice(double productPrice)
+	{
+		this.productPrice=productPrice;
+	}
+	public double getProductPrice()
+	{
+		return productPrice;
+	}
+}
